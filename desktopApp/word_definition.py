494235c0f16f098af8af7921f538b6619dc203e6
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-02 22:13:07 +0300

    I can search and display a definition but positioning of widgit is messed up so far

--- desktopApp/word_definition.py
+++ desktopApp/word_definition.py
@@ -6,48 +6,50 @@ from PyQt6.QtCore import Qt
 
 local_api_path=Path('./data.json')
 
-def search_for_a_word(word):
+def search_for_a_word():
     wanted_word=word.text()
+    print(wanted_word)
 # loop through data.json file 
     with open(local_api_path) as json_file:
         data=json.load(json_file)
     wanted_word=wanted_word.strip().lower() # strip whitespaces and make lowercase
     if wanted_word in data:
-        print('# '+wanted_word.title())
+        definitions=''
         for i,definition in enumerate (data[wanted_word],start=1):
-            print(f"{i}. {definition}")
-            print(message.setText(wanted_word))
+            definitions+=str(i)+". "+definition+"\n"
+            message.setText(definitions)
     else:
-        print(f"Did not found - {wanted_word}")
+        message.setText(f"Did not found - {wanted_word}")
 
 # Initial set up
 app=QApplication([])
 window=QWidget() # all widget, button will be added in this "window"
 window.setWindowTitle('Word Definition')
 
-main_layout=QHBoxLayout()
-
+main_layout=QVBoxLayout()
+window.setLayout(main_layout)
+def_layout=QHBoxLayout()
 # Input/Enter a word
-text=QLineEdit()
-main_layout.addWidget(text)
+word=QLineEdit()
+def_layout.addWidget(word)
 
 # adding another widget for a button
 btn=QPushButton('Find')
-main_layout.addWidget(btn)
+def_layout.addWidget(btn)
 # connecting a button to the txt field with a slot in it in its turn is a fn
 btn.clicked.connect(search_for_a_word)
 
 # adding another label/widget/filed to display the outcome
+main_layout.addLayout(def_layout)
 result_as_def=QLabel('')
-main_layout.addWidget(result_as_def)
+def_layout.addWidget(result_as_def)
 
 
 # adding widget/Label as msg to inform an user what's going on
-message=QLabel('test')
-main_layout.addWidget(message,alignment=Qt.AlignmentFlag.AlignCenter)
+
+message=QLabel('')
+def_layout.addWidget(message)#alignment=Qt.AlignmentFlag.AlignCenter
 
 
-
-window.setLayout(main_layout)
 window.show()
 app.exec()
